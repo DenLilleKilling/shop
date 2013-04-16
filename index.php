14232615549a4b92c4734e96d5ef2d20b2a8f001
Mille <DenLilleKilling_89@live.com>
2013-04-16 11:05:45 +0200

    det jeg har lavet  og billeder

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include ('includes/db_conncet.php');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<link reel="stylesheet" type="text/css" href="css/style.css"/>
+        <title>Burlesque shoppen</title>
+    </head>
+    <body>
+		<div id="wrapper">
+		<?php
+		// put your code here
+		?>
+		</div>
+    </body>
+</html>
